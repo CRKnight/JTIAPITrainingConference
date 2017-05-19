bba2bc7b24303e3efe546776aaef6318dfb444b6
Cameron Knight <cknight@journaltech.com>
2017-05-19 12:55:28 -0600

    Add API Library

---
+++ APITrainingConference/APILibrary/APIWorker.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APILibrary
+{
+	public class APIWorker
+	{
+		public void GetClient()
+		{
+			//Create client
+			//Add credentials 
+		}
+
+		private void GetName()
+		{
+			//Create Name
+		}
+
+		private void GetCase()
+		{
+			//Create and Submit 
+		}
+
+		private void GetCourtNumber()
+		{
+			//Find the Court Number
+		}
+
+		private void AddDocumentToCase()
+		{
+			
+		}
+
+		private void CopyCase()
+		{
+			
+		}
+
+		private void AddObligation()
+		{
+			
+		}
+
+		private void MakePayment()
+		{
+			
+		}
+
+		private void Retry()
+		{
+			
+		}
+	}
+}
