8812111c9f136655d9d69dd7d4a9a244c8bee687
Cameron Knight <cknight@journaltech.com>
2017-05-24 13:19:51 -0600

    add console application

--- APITrainingConference/APILibrary/APIWorker.cs
+++ APITrainingConference/APILibrary/APIWorker.cs
@@ -1,64 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace APILibrary
 {
 	public class APIWorker
 	{
-		public void GetClient()
-		{
-			//Create client
 
-			//Add credentials 
-		}
-
-		private void GetName()
-		{
-			//Create new Name
-
-			//Set Required Properties
-
-			//Set Operation 
-		}
-
-		private void GetCase()
-		{
-			//Create Case
-
-			//Submit 
-		}
-
-		private void GetCourtNumber()
-		{
-			//Find the Court Number
-		}
-
-		private void AddDocumentToCase()
-		{
-			
-		}
-
-		private void CopyCase()
-		{
-			
-		}
-
-		private void AddObligation()
-		{
-			
-		}
-
-		private void MakePayment()
-		{
-			
-		}
-
-		private void Retry()
-		{
-			
-		}
 	}
 }
