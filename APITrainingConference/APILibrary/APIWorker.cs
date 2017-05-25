c9d1310dd8fb11b176dcd1818c9d5a9cc3004d46
Cameron Knight <cknight@journaltech.com>
2017-05-25 08:52:07 -0600

    Add service reference, create client, add credentials, and get callerID

--- APITrainingConference/APILibrary/APIWorker.cs
+++ APITrainingConference/APILibrary/APIWorker.cs
@@ -14,9 +14,26 @@ namespace APILibrary
 		private readonly ILog _logger = LogFactory.CreateForType<ILog>();
 		private readonly CoreFilingMessage _message = XmlParser.CreateMessage(XML_DIRECTORY_PATH + "APIClass.xml");
 
+		private JustWareApiClient _client;
+
+		public APIWorker()
+		{
+			CreateApiClient();
+		}
+
 		public void LogXml()
 		{
 			_logger.Debug($"{_message.Case.CaseParticipants.FirstOrDefault().CaseParticipantRoleCode}");
+		}
+
+		private void CreateApiClient()
+		{
+			_client = new JustWareApiClient();
+			_client.ClientCredentials.UserName.UserName = "tc\\Admin";
+			_client.ClientCredentials.UserName.Password = "JustWare6";
+
+			int id = _client.GetCallerNameID();
+			_logger.Debug($"Caller Id = {id}");
 		}
 	}
 }
