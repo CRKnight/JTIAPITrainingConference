0fbc50f50f82c0c07f66e81dc535f5232a030bca
Ryan Hartley <rhartley@journaltech.com>
2017-05-30 16:48:02 -0600

    Added error handling to xmlparser

--- APITrainingConference/APITrainingUtilities/Xml/XmlParser.cs
+++ APITrainingConference/APITrainingUtilities/Xml/XmlParser.cs
@@ -1,12 +1,20 @@
 using System.IO;
 using System.Xml.Serialization;
+using APITrainingUtilities.Logging;
 
 namespace APITrainingUtilities.Xml
 {
-	public static class XmlParser
+	public class XmlParser
 	{
+		private static readonly ILog _logger = LogFactory.CreateForType<XmlParser>();
+
 		public static CoreFilingMessage CreateMessage(string path)
 		{
+			if (!File.Exists(path))
+			{
+				_logger.Error("File does not exist: {Path}", path);
+				return null;
+			}
 			var xmlSerializer = new XmlSerializer(typeof(CoreFilingMessage));
 			using (StreamReader streamReader = new StreamReader(path))
 			{
