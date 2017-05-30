5ffc35a0f79cf2d974288341778a6ee3984f37e1
Ryan Hartley <rhartley@journaltech.com>
2017-05-30 16:48:31 -0600

    After Lab2

--- APITrainingConference/APITrainingConference/Program.cs
+++ APITrainingConference/APITrainingConference/Program.cs
@@ -1,14 +1,22 @@
 ﻿using System;
+using APITrainingUtilities.Logging;
+using APITrainingUtilities.Xml;
 
 namespace APITrainingConference
 {
-	static class Program
+	class Program
 	{
+		private static readonly ILog _logger = LogFactory.CreateForType<Program>();
+		private const string SAMPLE_1_PATH = @"Data\Sample1.xml";
+
 		/// <summary>
 		/// The main entry point for the application.
 		/// </summary>
 		static void Main()
 		{
+			_logger.Debug("Log file location: {0}", LogFactory.LogFilePath);
+			CoreFilingMessage sample1 = XmlParser.CreateMessage(SAMPLE_1_PATH);
+			_logger.Information("Processing CaseDocketID: {0}", sample1.Case.CaseDocketID);
 			Console.WriteLine("Press enter to exit");
 			Console.ReadLine();
 		}
